75dca3bdc6cc265ead8d7e1dec3b147b8d751c8a
Lacrotte Florent <lacrotte.florent@gmail.com>
2020-06-08 09:14:12 +0200

    create shop controller and design shops view

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -4,23 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Shop;
 
 class ShopController extends AbstractController
 {
     /**
-     * @Route("/shop", name="shop_show")
+     * @Route("/shop{id}", name="shop_show")
      */
     public function show()
     {
-
-        $repo = $this->getDoctrine()->getRepository(Shop::class);
-
-        $shops = $repo->findAll();
-        //dd($shops);
-
-        return $this->render('shop/shop.html.twig', [
-            'shops'      => $shops,
+        return $this->render('shop/index.html.twig', [
+            'controller_name' => 'ShopController',
         ]);
     }
 }
---
+++ src/Controller/ShopsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Shop;
+
+class ShopsController extends AbstractController
+{
+    /**
+     * @Route("/shops", name="shops_show")
+     */
+    public function show()
+    {
+
+        $repo = $this->getDoctrine()->getRepository(Shop::class);
+
+        $shops = $repo->findAll();
+
+        return $this->render('shops/shops.html.twig', [
+            'shops'      => $shops,
+        ]);
+    }
+}
